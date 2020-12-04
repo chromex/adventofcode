1a431a803b0318f4f80d0c1d6ef3f5dad4903c82
Loren Hoffman <loren.hoffman@gmail.com>
2020-12-03 20:18:15 -0800

    Stub out day 4

---
+++ AdventOfCode/Day_04.cs
@@ -0,0 +1,25 @@
+﻿using AoCHelper;
+using System.IO;
+
+namespace AdventOfCode
+{
+    public class Day_04 : BaseDay
+    {
+        private readonly string _input;
+
+        public Day_04()
+        {
+            _input = File.ReadAllText(InputFilePath);
+        }
+
+        public override string Solve_1()
+        {
+            return "1337";
+        }
+
+        public override string Solve_2()
+        {
+            return "1337";
+        }
+    }
+}
--- AdventOfCode/Day_26.cs
+++ AdventOfCode/Day_26.cs
@@ -3,17 +3,17 @@ using System.IO;
 
 namespace AdventOfCode
 {
-    public class Day_26 : BaseDay
-    {
-        private readonly string _input;
+    //public class Day_26 : BaseDay
+    //{
+    //    private readonly string _input;
 
-        public Day_26()
-        {
-            _input = File.ReadAllText(InputFilePath);
-        }
+    //    public Day_26()
+    //    {
+    //        _input = File.ReadAllText(InputFilePath);
+    //    }
 
-        public override string Solve_1() => _input.Length.ToString();
+    //    public override string Solve_1() => _input.Length.ToString();
 
-        public override string Solve_2() => throw new System.NotImplementedException();
-    }
+    //    public override string Solve_2() => throw new System.NotImplementedException();
+    //}
 }
