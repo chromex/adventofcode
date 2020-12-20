f6a79792f4c9d33a23dae4124d758c0f26cc57b4
Loren Hoffman <loren.hoffman@gmail.com>
2020-12-19 18:58:02 -0800

    Cleaned up day 19

--- AdventOfCode/Day_19.cs
+++ AdventOfCode/Day_19.cs
@@ -6,20 +6,15 @@ namespace AdventOfCode
 {
     public class Day_19 : BetterBaseDay
     {
-        private string[] rules;
+        private Dictionary<int, string> rules;
 
         public override string Solve_1()
         {
             rules = Input
                 .Where(line => line.Contains(":"))
-                .Select(line =>
-                {
-                    int idx = line.IndexOf(":");
-                    return Tuple.Create(int.Parse(line.Substring(0, idx)), line.Substring(idx + 2));
-                })
-                .OrderBy(rule => rule.Item1)
-                .Select(rule => rule.Item2)
-                .ToArray();
+                .ToDictionary(
+                    line => int.Parse(line.Substring(0, line.IndexOf(":"))), 
+                    line => line.Substring(line.IndexOf(":") + 2));
 
             return Input.Where(line => !line.Contains(":") && !string.IsNullOrEmpty(line) && Check(line, rules[0]) == line.Length).Count().ToString();
         }
@@ -30,11 +25,9 @@ namespace AdventOfCode
 
             if (pipeIndex >= 0)
             {
-                int left = Check(line, rule.Substring(0, pipeIndex - 1));
-                if (left > 0)
-                    return left;
-                else
-                    return Check(line, rule.Substring(pipeIndex + 2, rule.Length - pipeIndex - 2));
+                return Math.Max(
+                    Check(line, rule.Substring(0, pipeIndex - 1)), 
+                    Check(line, rule.Substring(pipeIndex + 2, rule.Length - pipeIndex - 2)));
             }
             else
             {
@@ -46,24 +39,21 @@ namespace AdventOfCode
                     {
                         return line[0] == split[0][1] ? 1 : 0;
                     }
-                    else
-                    {
-                        return Check(line, rules[int.Parse(split[0])]);
-                    }
+
+                    return Check(line, rules[int.Parse(split[0])]);
                 }
                 else
                 {
-                    int amt = 0;
+                    int amt = 0, c;
                     for (int idx = 0; idx < split.Length; ++idx)
                     {
                         if (amt >= line.Length)
                             return 0;
 
-                        int c = Check(line.Substring(amt), split[idx]);
+                        amt += c = Check(line.Substring(amt), split[idx]);
+
                         if (c == 0)
                             return 0;
-                        else
-                            amt += c;
                     }
                     return amt;
                 }
@@ -73,33 +63,26 @@ namespace AdventOfCode
         private int Override(string line)
         {
             Dictionary<int, int> count = new Dictionary<int, int>() { { 42, 0 }, { 31, 0 } };
-            int rule = 42;
-            int offset = 0;
+            int rule = 42, offset = 0, r;
 
             while (offset < line.Length)
             {
-                int r = Check(line.Substring(offset), rules[rule]);
+                offset += r = Check(line.Substring(offset), rules[rule]);
                 if (r > 0)
                 {
                     ++count[rule];
-                    offset += r;
                 }
                 else if (rule == 42)
                 {
                     rule = 31;
                 }
-                else
+                else 
                 {
                     break;
                 }
             }
 
-            if (count[31] > 0 && count[42] > 1 && count[31] < count[42])
-            {
-                return offset;
-            }
-
-            return 0;
+            return (count[31] > 0 && count[31] < count[42]) ? offset : 0;
         }
 
         public override string Solve_2()
