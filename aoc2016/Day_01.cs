0457c52d91610b0fcc52a88dfb1f123395f61bd5
Loren Hoffman <loren.hoffman@gmail.com>
2021-10-25 16:12:04 -0700

    Adding 2016 project

---
+++ aoc2016/Day_01.cs
@@ -0,0 +1,17 @@
+﻿using AoCUtil;
+
+namespace aoc2016
+{
+    class Day_01 : BetterBaseDay
+    {
+        public override string Solve_1()
+        {
+            return "noa";
+        }
+
+        public override string Solve_2()
+        {
+            return "no";
+        }
+    }
+}
