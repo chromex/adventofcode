d5a190f01942536c408a7a00d5e7205456a471ea
Loren Hoffman <loren.hoffman@gmail.com>
2021-10-25 21:02:15 -0700

    Days 3 and 4

---
+++ aoc2016/Day_03.cs
@@ -0,0 +1,47 @@
+﻿using AoCUtil;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace aoc2016
+{
+    class Day_03 : BetterBaseDay
+    {
+        public override string Solve_1()
+        {
+            int n = 0;
+            Input.Select(line => line.Split(" ", System.StringSplitOptions.RemoveEmptyEntries)).ForEach(spl =>
+            {
+                int[] sides = spl.Select(ent => int.Parse(ent)).OrderBy(v => v).ToArray();
+
+                if (sides[0] + sides[1] > sides[2])
+                    ++n;
+            });
+            return n.ToString();
+        }
+
+        private static bool Valid(string[][] input, int row, int col)
+        {
+            List<int> set = new();
+            set.Add(int.Parse(input[row][col]));
+            set.Add(int.Parse(input[row + 1][col]));
+            set.Add(int.Parse(input[row + 2][col]));
+            set = set.OrderBy(v => v).ToList();
+
+            return set[0] + set[1] > set[2];
+        }
+
+        public override string Solve_2()
+        {
+            string[][] input = Input.Select(line => line.Split(" ", System.StringSplitOptions.RemoveEmptyEntries)).ToArray();
+
+            int n = 0;
+            for (int row = 0; row < Input.Length; row += 3)
+            {
+                if (Valid(input, row, 0)) ++n;
+                if (Valid(input, row, 1)) ++n;
+                if (Valid(input, row, 2)) ++n;
+            }
+            return n.ToString();
+        }
+    }
+}
---
+++ aoc2016/Day_04.cs
@@ -0,0 +1,102 @@
+﻿using AoCUtil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace aoc2016
+{
+    class Day_04 : BetterBaseDay
+    {
+        private static string Checksum(Dictionary<char, int> counts)
+        {
+            int max = counts.Values.Max();
+
+            string check = "";
+
+            while (check.Length < 5)
+            {
+                foreach (var entry in counts)
+                {
+                    if (entry.Value == max)
+                    {
+                        check += entry.Key;
+                    }
+                }
+
+                max = Math.Max(0, max - 1);
+            }
+
+            return check.Substring(0, 5);
+        }
+
+        private static bool IsReal(string str, out int sector)
+        {
+            string front = str.Substring(0, str.Length - 7);
+            string check = str.Substring(str.Length - 6, 5);
+
+            Dictionary<char, int> counts = new();
+            for (char c = 'a'; c <= 'z'; ++c)
+            {
+                counts[c] = 0;
+            }
+
+            front.ForEach(ch =>
+            {
+                if (char.IsLetter(ch)) { ++counts[ch]; }
+            });
+
+            sector = int.Parse(front.Substring(front.LastIndexOf('-') + 1));
+
+            return check == Checksum(counts);
+        }
+
+        public override string Solve_1()
+        {
+            ulong sum = 0;
+            int sector = 0;
+            Input.ForEach(line =>
+            {
+                if (IsReal(line, out sector))
+                {
+                    sum += (ulong)sector;
+                }
+            });
+            return sum.ToString();
+        }
+
+        private static char Shift(char c, int count)
+        {
+            if (c == '-') return ' ';
+
+            int v = c - 'a';
+            v = (v + count) % 26;
+            return (char)('a' + v);
+        }
+
+        private static string GetRealName(string str, int sector)
+        {
+            string front = str.Substring(0, str.LastIndexOf("-"));
+            return new string(front.Select(ch => Shift(ch, sector)).ToArray());
+        }
+
+        public override string Solve_2()
+        {
+            int result = 0;
+
+            Input.ForEach(line =>
+            {
+                int sector = 0;
+                if (IsReal(line, out sector))
+                {
+                    Console.WriteLine(GetRealName(line, sector));
+                    if (GetRealName(line, sector).Contains("north"))
+                    {
+                        result = sector;
+                    }
+                }
+            });
+
+            return result.ToString();
+        }
+    }
+}
