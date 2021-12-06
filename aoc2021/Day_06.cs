6938125c04a4730dd3c49db53fc4e6484d3fc072
Loren Hoffman <loren.hoffman@gmail.com>
2021-12-05 22:08:49 -0800

    6

--- aoc2021/Day_06.cs
+++ aoc2021/Day_06.cs
@@ -1,22 +1,35 @@
 ﻿using AoCUtil;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace aoc2021
 {
     class Day_06 : BetterBaseDay
     {
+        private string Run(int days)
+        {
+            ulong[] numAge = new ulong[9];
+
+            Input[0].Split(',').ForEach(s => ++numAge[s.AsInt()]);
+
+            for (int d = 0; d < days; ++d)
+            {
+                ulong nZero = numAge[0];
+                Util.LeftShiftArray(numAge, 1);
+                numAge[6] += nZero;
+            }
+
+            ulong sum = 0;
+            numAge.ForEach(a => sum += a);
+            return sum.ToString();
+        }
+
         public override string P1()
         {
-            return "no";
+            return Run(80);
         }
 
         public override string P2()
         {
-            return "no";
+            return Run(256);
         }
     }
 }
