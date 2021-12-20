92255653240d122b77ffe8cc3f6a3fa640a6847f
Loren Hoffman <loren.hoffman@gmail.com>
2021-12-19 22:13:06 -0800

    Day 20

---
+++ aoc2021/Day_20.cs
@@ -0,0 +1,113 @@
+﻿using AoCUtil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc2021
+{
+    class Day_20 : BetterBaseDay
+    {
+        private static void Print(Matrix<char> map)
+        {
+            StringBuilder sb = new();
+
+            for (int row = 0; row < map.Height; ++row)
+            {
+                Console.WriteLine(new string(map.GetRow(row)));
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("wat");
+        }
+
+        private static Matrix<char> Apply(Matrix<char> map, string conversion)
+        {
+            Matrix<char> ret = new(map.Width, map.Height, '.');
+
+            char def = map.Data[0, 0] == '#' ? '.' : '#';
+
+            ret.ForEachCoord((x, y) =>
+            {
+                if (x == 0 || x == map.Width - 1 || y == 0 || y == map.Height - 1)
+                {
+                    ret.Data[x, y] = def;
+                    return;
+                }
+
+                StringBuilder binary = new();
+                binary.Append(map.Data[x - 1, y - 1] == '#' ? 1 : 0);
+                binary.Append(map.Data[x, y - 1] == '#' ? 1 : 0);
+                binary.Append(map.Data[x + 1, y - 1] == '#' ? 1 : 0);
+
+                binary.Append(map.Data[x - 1, y] == '#' ? 1 : 0);
+                binary.Append(map.Data[x, y] == '#' ? 1 : 0);
+                binary.Append(map.Data[x + 1, y] == '#' ? 1 : 0);
+
+                binary.Append(map.Data[x - 1, y + 1] == '#' ? 1 : 0);
+                binary.Append(map.Data[x, y + 1] == '#' ? 1 : 0);
+                binary.Append(map.Data[x + 1, y + 1] == '#' ? 1 : 0);
+
+                ret.Data[x, y] = conversion[(int)Util.ParseBinary(binary.ToString())];
+            });
+
+            return ret;
+        }
+
+        public override string P1()
+        {
+            string conversion = Input[0];
+            Matrix<char> map = new(120, 120, '.');
+
+            for (int l = 2; l < Input.Length; ++l)
+            {
+                for (int ch = 0; ch < Input[l].Length; ++ch)
+                {
+                    map.Data[10 + ch, 8 + l] = Input[l][ch];
+                }
+            }
+
+            map = Apply(map, conversion);
+            map = Apply(map, conversion);
+
+            int sum = 0;
+            for (int x = 3; x < map.Width - 3; ++x)
+            {
+                for (int y = 3; y < map.Height - 3; ++y)
+                {
+                    if (map.Data[x, y] == '#') ++sum;
+                }
+            }
+
+            return sum.ToString();
+        }
+
+        public override string P2()
+        {
+            string conversion = Input[0];
+            Matrix<char> map = new(250, 250, '.');
+
+            for (int l = 2; l < Input.Length; ++l)
+            {
+                for (int ch = 0; ch < Input[l].Length; ++ch)
+                {
+                    map.Data[75 + ch, 73 + l] = Input[l][ch];
+                }
+            }
+
+            for (int i = 0; i < 50; ++i) map = Apply(map, conversion);
+
+            int sum = 0;
+            for (int x = 3; x < map.Width - 3; ++x)
+            {
+                for (int y = 3; y < map.Height - 3; ++y)
+                {
+                    if (map.Data[x, y] == '#') ++sum;
+                }
+            }
+
+            return sum.ToString();
+        }
+    }
+}
