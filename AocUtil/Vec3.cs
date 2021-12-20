65e6707d055a7c1fe4da556526b31cea003ff990
Loren Hoffman <loren.hoffman@gmail.com>
2021-12-19 20:29:40 -0800

    Day 19 wat

---
+++ AocUtil/Vec3.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace AoCUtil
+{
+    public class Vec3
+    {
+        public int X;
+        public int Y;
+        public int Z;
+
+        public Vec3()
+        { }
+
+        public Vec3(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public Vec3(string str)
+        {
+            string[] spl = str.Split(',');
+            X = spl[0].AsInt();
+            Y = spl[1].AsInt();
+            Z = spl[2].AsInt();
+        }
+
+        public override string ToString() => $"({X}, {Y}, {Z})";
+
+        public static Vec3 operator+(Vec3 lhs, Vec3 rhs)
+        {
+            return new Vec3(lhs.X + rhs.X, lhs.Y + rhs.Y, lhs.Z + rhs.Z);
+        }
+
+        public static Vec3 operator-(Vec3 lhs, Vec3 rhs)
+        {
+            return new Vec3(lhs.X - rhs.X, lhs.Y - rhs.Y, lhs.Z - rhs.Z);
+        }
+
+        public static bool operator==(Vec3 lhs, Vec3 rhs)
+        {
+            return lhs.X == rhs.X && lhs.Y == rhs.Y && lhs.Z == rhs.Z;
+        }
+
+        public static bool operator!=(Vec3 lhs, Vec3 rhs)
+        {
+            return lhs.X != rhs.X || lhs.Y != rhs.Y || lhs.Z != rhs.Z;
+        }
+
+        public int Manhattan => Math.Abs(X) + Math.Abs(Y) + Math.Abs(Z);
+
+        public void RotateAroundX()
+        {
+            int tmp = Z;
+            Z = Y;
+            Y = -tmp;
+        }
+
+        public void RotateAroundY()
+        {
+            int tmp = X;
+            X = Z;
+            Z = -tmp;
+        }
+
+        public void RotateAroundZ()
+        {
+            int tmp = Y;
+            Y = X;
+            X = -tmp;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(obj, null))
+            {
+                return false;
+            }
+
+            if (obj is Vec3)
+            {
+                return this == (obj as Vec3);
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode() => (X, Y, Z).GetHashCode();
+    }
+}
---
+++ aoc2021/Day_19.cs
@@ -0,0 +1,174 @@
+﻿using AoCUtil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc2021
+{
+    class Day_19 : BetterBaseDay
+    {
+        class Scanner
+        {
+            public int id;
+            public List<Vec3> beacons = new();
+            public bool marked;
+            public Vec3 position = new();
+
+            public void RotateX() => beacons.ForEach(v => v.RotateAroundX());
+            public void RotateY() => beacons.ForEach(v => v.RotateAroundY());
+            public void RotateZ() => beacons.ForEach(v => v.RotateAroundZ());
+        }
+
+        private static List<Scanner> Load(string[] input)
+        {
+            List<Scanner> res = new();
+
+            Scanner current = null;
+            input.ForEach(line =>
+            {
+                if (current == null)
+                {
+                    current = new();
+                    res.Add(current);
+                    string[] spl = line.Split(' ');
+                    current.id = spl[2].AsInt();
+                }
+                else if (line.Contains(','))
+                {
+                    current.beacons.Add(new(line));
+                }
+                else
+                {
+                    current = null;
+                }
+            });
+
+            return res;
+        }
+
+        private static bool Intersect(List<Vec3> leftBeacons, List<Vec3> rightBeacons, Vec3 offset)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < leftBeacons.Count; ++i)
+            {
+                Vec3 l = leftBeacons[i];
+
+                for (int j = 0; j < rightBeacons.Count; ++j)
+                {
+                    Vec3 r = rightBeacons[j];
+
+                    if (l.X == (r.X + offset.X) &&
+                        l.Y == (r.Y + offset.Y) &&
+                        l.Z == (r.Z + offset.Z))
+                    {
+                        ++sum;
+                        break;
+                    }
+                }
+
+            }
+
+            return sum >= 12;
+        }
+
+        private static bool Overlap(Scanner left, Scanner right, out Vec3 offset)
+        {
+            offset = new();
+
+            for (int i = 0; i < left.beacons.Count; ++i)
+            {
+                Vec3 l = left.beacons[i];
+
+                for (int j = 0; j < right.beacons.Count; ++j)
+                {
+                    Vec3 r = right.beacons[j];
+                    offset = l - r;
+
+                    if (Intersect(left.beacons, right.beacons, offset))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private static bool FindOverlap(Scanner left, Scanner right, HashSet<Vec3> uniques)
+        {
+            for (int x = 0; x < 4; ++x)
+            {
+                for (int y = 0; y < 4; ++y)
+                {
+                    for (int z = 0; z < 4; ++z)
+                    {
+                        Vec3 offset;
+                        if (Overlap(left, right, out offset))
+                        {
+                            Console.Write(".");
+                            right.beacons = right.beacons.Select(p => p + offset).ToList();
+                            right.beacons.ForEach(p => uniques.Add(p));
+                            right.marked = true;
+                            right.position = offset;
+                            return true;
+                        }
+
+                        right.RotateZ();
+                    }
+
+                    right.RotateY();
+                }
+
+                right.RotateX();
+            }
+
+            return false;
+        }
+
+        List<Scanner> scanners;
+
+        public override string P1()
+        {
+            scanners = Load(Input);
+
+            HashSet<Vec3> uniques = new();
+            scanners[0].beacons.ForEach(p => uniques.Add(p));
+            scanners[0].marked = true;
+
+            while (scanners.Any(s => !s.marked))
+            {
+                foreach (Scanner right in scanners.Where(s => !s.marked))
+                {
+                    foreach (Scanner left in scanners.Where(s => s.marked))
+                    {
+                        if (FindOverlap(left, right, uniques))
+                        {
+                            Console.WriteLine($"Overlap: {left.id} {right.id}");
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return uniques.Count.ToString();
+        }
+
+        public override string P2()
+        {
+            int max = 0;
+
+            for (int i = 0; i < scanners.Count - 1; ++i)
+            {
+                for (int j = i + 1; j < scanners.Count; ++j)
+                {
+                    max = Math.Max(max, (scanners[i].position - scanners[j].position).Manhattan);
+                }
+            }
+
+            return max.ToString();
+        }
+    }
+}
