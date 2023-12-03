9f62333c88f39deb8607314f526ce4d957a074bc
Loren Hoffman <loren.hoffman@gmail.com>
2023-12-03 10:58:24 -0800

    2023 begins

---
+++ aoc2023/aoc.py
@@ -0,0 +1,115 @@
+def Pairwise(fn, lstA, lstB):
+    return list(fn(x, lstB[ind]) for ind,x in enumerate(lstA))
+
+class DataMatrix:
+    marks = None
+
+    def __init__(self, data: str):
+        self.raw = data
+        self.lines = data.splitlines()
+        self.__height = len(self.lines)
+        self.__width = len(self.lines[0])
+
+    def __getitem__(self, key):
+        """
+        [y] returns row y
+        [x, y] returns item x in row y
+        [x:z, y] returns a slice of items from x to z (exclusive) on row y
+        """
+        if type(key) is int:
+            return self.lines[key]
+
+        if type(key) is tuple:
+            return self.lines[key[1]][key[0]]
+        
+    def __str__(self) -> str:
+        output = []
+
+        for y in range(self.__height):
+            if self.marks is None:
+                output.append(self.lines[y])
+            else:
+                output.append(f"{self.lines[y]} | {self.PrintMarks_Line(y)}")
+
+        return "\n".join(output)
+    
+    @property
+    def Height(self): return self.__height
+
+    @property
+    def Width(self): return self.__width
+
+    def IndexInBounds(self, x, y):
+        return x >= 0 and y >= 0 and x < self.Width and y < self.Height
+    
+    def Spread(self, x, y, callback):
+        width = self.Width
+        if y > 0:
+            if x > 0:
+                callback(x - 1, y - 1)
+            callback(x    , y - 1)
+            if x < width:
+                callback(x + 1, y - 1)
+        if x > 0:
+            callback(x - 1, y)
+        if x < width:
+            callback(x + 1, y)
+        if y < self.Height:
+            if x > 0:
+                callback(x - 1, y + 1)
+            callback(x    , y + 1)
+            if x < width:
+                callback(x + 1, y + 1)
+
+    def Scan(self, pred, callback):
+        for y in range(self.Height):
+            for x in range(self.Width):
+                val = self.lines[y][x]
+                if pred(val):
+                    callback(val, x, y)
+
+    def ScanMarks(self, callback):
+        result = []
+        if self.marks != None:
+            for y in range(self.Height):
+                if self.marks[y] != None:
+                    for x in range(self.Width):
+                        if self.IsMarked(x, y):
+                            result.append(callback(self.lines[y][x], x, y))
+        return result
+        
+    def Mark(self, x:int, y:int):
+        if self.marks == None:
+            self.marks = [None] * self.__height
+
+        if self.marks[y] == None:
+            self.marks[y] = [None] * self.__width
+
+        self.marks[y][x] = True
+
+    def ClearMark(self, x:int, y:int):
+        if self.marks != None and self.marks[y] != None:
+            self.marks[y][x] = False
+    
+    def ResetMarks(self):
+        self.marks = None
+
+    def IsMarked(self, x:int, y:int) -> bool:
+        if self.marks != None and self.marks[y] != None:
+            return self.marks[y][x] is True
+        return False
+            
+    def PrintMarks(self):
+        if self.marks is None:
+            print("no marks")
+            return
+        
+        for y in range(self.__height):
+            print(self.PrintMarks_Line(y))
+
+    def PrintMarks_Line(self, y: int):
+        line = self.marks[y]
+        if line is None:
+            return "." * self.__width
+        else:
+            return "".join(map(lambda item : "#" if item is True else ".", line))
---
+++ aoc2023/day3.py
@@ -0,0 +1,52 @@
+import aoc
+
+input = """467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598.."""
+
+matrix = aoc.DataMatrix(input)
+
+def Mark(x, y):
+    if str.isdigit(matrix[x, y]):
+        matrix.Mark(x, y)
+        lx = x - 1
+        while matrix.IndexInBounds(lx, y) and str.isdigit(matrix[lx, y]):
+            matrix.Mark(lx, y)
+            lx = lx - 1
+        lx = x + 1
+        while matrix.IndexInBounds(lx, y) and str.isdigit(matrix[lx, y]):
+            matrix.Mark(lx, y)
+            lx = lx + 1
+
+def GetNum(val, x, y):
+    num = 0
+    while matrix.IndexInBounds(x, y) and str.isdigit(matrix[x, y]):
+        num = (num * 10) + int(matrix[x, y])
+        matrix.ClearMark(x, y)
+        x = x + 1
+    return num
+
+sum = 0
+
+def MarkNumbers(val, x, y):
+    global sum
+
+    matrix.Spread(x, y, Mark)
+
+    nums = matrix.ScanMarks(GetNum)
+    matrix.ResetMarks()
+
+    if len(nums) == 2:
+        sum = sum + (nums[0] * nums[1])
+
+matrix.Scan(lambda x: x == "*", MarkNumbers)
+
+print(sum)
+print(matrix)
